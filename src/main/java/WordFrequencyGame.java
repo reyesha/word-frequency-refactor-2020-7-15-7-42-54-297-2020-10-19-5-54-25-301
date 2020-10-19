602996965ef0c980f2b323379489b21ab93adfa1
Hannah Mae Reyes <hannah.mae.reyes@oocl.com>
2020-10-19 14:46:57 +0800

    [WordFrequency] [Hannah] refactor: rename s to wordInfoLine

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -33,8 +33,8 @@ public class WordFrequencyGame {
 
                 StringJoiner joiner = new StringJoiner("\n");
                 for (WorldInfo wordInfo : worldInfoList) {
-                    String s = wordInfo.getValue() + " " +wordInfo.getWordCount();
-                    joiner.add(s);
+                    String wordInfoLine = wordInfo.getValue() + " " +wordInfo.getWordCount();
+                    joiner.add(wordInfoLine);
                 }
                 return joiner.toString();
             } catch (Exception e) {
